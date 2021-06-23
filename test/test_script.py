4e59a2009d97c399ab3b4a0f419ef2e184eaaf01
Hung Do <hungdojan@gmail.com>
2021-06-23 22:40:15 +0200

    Add advance tests and test files

--- test/test_script.py
+++ test/test_script.py
@@ -4,7 +4,7 @@
 #  @author Hung Do
 from sortsubj import *
 import pytest
-import unittest
+import os
 
 def test_empty_obj():
     s = SubSort()
@@ -143,6 +143,90 @@ def test_delete_day():
     
     assert len(s.days) == 0
 
+def test_load_file_student():
+    s: SubSort = SubSort()
+
+    # prazdna cesta
+    with pytest.raises(Exception):
+        s.load_file_student(None)
+    # spatny format/typ
+    with pytest.raises(Exception):
+        s.load_file_student("my_file.txt")
+    # neexistujici soubor
+    with pytest.raises(Exception):
+        s.load_file_student("my_file.csv")
+
+    s.load_file_student("input_zaci.csv")
+    assert len(s.students) == 10
+    student1 = s.students["1"]
+    student5 = s.students["5"]
+    student10 = s.students["10"]
+
+    assert student1.first_name == "Adéla"
+    assert student5.first_name == "Adéla"
+    assert student10.first_name == "Pavel"
+
+    assert s.students["3"].first_name == "Ondřej"
+
+    assert student1.subjects[0] == "ZSV"
+    assert student5.subjects[2] == "Nj-DSD1"
+    assert student10.subjects[1] == "Aj-FCE"
+
+def test_load_file_subjects():
+    s: SubSort = SubSort()
+
+    # prazdna cesta
+    with pytest.raises(Exception):
+        s.load_file_subjects(None)
+    # spatny format/typ
+    with pytest.raises(Exception):
+        s.load_file_subjects("my_file.txt")
+    # neexistujici soubor
+    with pytest.raises(Exception):
+        s.load_file_subjects("my_file.csv")
+
+    s.load_file_subjects("input_predmety.csv")
+    assert len(s.subject) == 14
+    assert "ZSV" in s.subject
+    assert "M-VŠ" in s.subject
+    assert not "zsv" in s.subject
+    assert not "Aj-fce" in s.subject
+
+def test_load_day_file():
+    s: SubSort = SubSort()
+    s.load_file_subjects("input_predmety.csv")
+    # prazdna cesta
+    with pytest.raises(Exception):
+        s.load_file_days(None)
+    # spatny format/typ
+    with pytest.raises(Exception):
+        s.load_file_days("my_file.txt")
+    # neexistujici soubor
+    with pytest.raises(Exception):
+        s.load_file_days("my_file.csv")
+
+    s.load_file_days("input_dny.csv")
+    assert len(s.days) == 3
+    assert "ZSV" in s.days[2]
+    assert "ZSV" in s.days[1]
+    assert not "ZSV" in s.days[0]
+    for subs in s.days:
+        assert len(subs) == 6
+
+    assert "Pr" in s.days[2]
+    assert "Fy" in s.days[1]
+    assert "Aj-Konv" in s.days[0]
+
+def test_generate_files():
+    s: SubSort = SubSort()
+    s.generate_files()
+    counter = 0
+    for file in os.listdir(os.getcwd()):
+        if os.path.isfile(file) and file.startswith("output"):
+            counter += 1
+    
+    assert counter >= 6
+
 @pytest.fixture()
 def setup_manual():
     s = SubSort()
@@ -151,8 +235,9 @@ def setup_manual():
     s.add_subject("s2")
     s.add_subject("s3")
     s.add_subject("s4")
+    s.add_subject("s5")
 
-    s.add_day({"s1", "s2", "s3"})
+    s.add_day({"s1", "s2", "s3", "s5"})
     s.add_day({"s2", "s4"})
     s.add_day({"s1", "s4"})
 
@@ -162,8 +247,8 @@ def setup_manual():
     s.students[0].subjects = ("s1")
     s.students[1].subjects = ("s1", "s2")
     s.students[2].subjects = ("s1", "s4")
-    s.students[3].subjects = ("s4", "s2", "s1")
-    s.students[4].subjects = ("s2", "s3", "s4")
+    s.students[3].subjects = ("s3", "s2", "s1")
+    s.students[4].subjects = ("s5", "s3", "s4")
     yield s
 
 def test_clear(setup_manual):
@@ -178,3 +263,65 @@ def test_clear(setup_manual):
     assert len(s.days) == 0
     assert len(s.subject) == 0
     assert s.nof_sorted_students == 0
+
+def test_sort_data(setup_manual):
+    s: SubSort = setup_manual
+
+    s.sort_data()
+
+    assert len(s.students[0].pass_subj) == 2
+    assert ("s1", None, None) in s.students[0].pass_subj
+    assert (None, None, "s1") in s.students[0].pass_subj
+    assert s.students[0].sel_subj == None
+    assert s.students[0].sorted
+
+    assert len(s.students[1].pass_subj) == 2
+    assert ("s1", "s2", None) in s.students[1].pass_subj
+    assert ("s2", None, "s1") in s.students[1].pass_subj
+    assert s.students[1].sel_subj == None
+    assert s.students[1].sorted
+
+    assert len(s.students[2].pass_subj) == 3
+    assert ("s1", "s4", None) in s.students[2].pass_subj
+    assert ("s1", None, "s4") in s.students[2].pass_subj
+    assert (None, "s4", "s1") in s.students[2].pass_subj
+    assert s.students[2].sel_subj == None
+    assert s.students[2].sorted
+
+    assert len(s.students[3].pass_subj) == 1
+    assert ("s3", "s2", "s1") in s.students[3].pass_subj
+    assert s.students[3].sel_subj == ("s3", "s2", "s1")
+    assert s.students[3].sorted
+
+    assert len(s.students[4].pass_subj) == 0
+    assert s.students[4].sel_subj == None
+    assert s.students[4].sorted
+
+    assert s.nof_sorted_students == 1
+
+def test_set_sel_subj(setup_manual):
+    s: SubSort = setup_manual
+
+    s.sort_data()
+
+    assert s.nof_sorted_students == 1
+
+    with pytest.raises(Exception):
+        s.student_sel_subject(-1, 0)
+    with pytest.raises(Exception):
+        s.student_sel_subject(10, 1)
+    with pytest.raises(Exception):
+        s.student_sel_subject(None, 0)
+
+    s.student_sel_subject(0, 2)
+    s.student_sel_subject(0, -1)
+
+    assert s.nof_sorted_students == 1
+    s.student_sel_subject(0, 1)
+
+    assert s.nof_sorted_students == 2
+    s.student_sel_subject(1, 1)
+    assert s.nof_sorted_students == 3
+
+    s.student_sel_subject(0, 0)
+    assert s.nof_sorted_students == 3
